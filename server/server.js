7ac22f0f396ea01b11a5c2a139c3f042c3033719
Devon Koch <devon.r.koch@gmail.com>
2015-08-06 12:20:36 -0700

    initialized server for rendering test asset (cat)

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
 var express = require('express');
+var app = express();
+var http = require('http');
+var path = require('path');
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+server.listen(port);
