23bed87a69055b575f101291153890fa62f8955c
Devon Koch <devon.r.koch@gmail.com>
2015-08-06 15:16:39 -0700

    added sample platform image

--- client/js/game.js
+++ client/js/game.js
@@ -1,8 +1,24 @@
 var preload = function(){
   console.log('preload working');
+
+  game.load.image('cat', 'assets/placeholder.png');
+  game.load.image('platform', 'assets/platform.png');
+
 };
 var create = function(){
   console.log('create working');
+  
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  var player = game.add.sprite(0, 0, 'cat');
+  var platform = game.add.sprite(400, 600, 'platform');
+  
+  game.physics.arcade.enable(player);
+  game.physics.arcade.enable(platform);
+
+  player.body.gravity.y = 980;
+  player.body.gravity.y = 980;
+
 };
 var update = function(){
   console.log('update working');
