10b906de14e30cf139a5e099be711f4ccbcb8bcd
Devon Koch <devon.r.koch@gmail.com>
2015-08-06 17:14:22 -0700

    added player movement:   -right   -left   -jump

--- client/js/preload.js
+++ client/js/preload.js
@@ -1,7 +1,7 @@
 var preload = function(){
   console.log('preload working');
 
-  game.load.image('cat', 'assets/chicken.png');
+  game.load.image('chicken', 'assets/chicken.png');
   game.load.image('platform', 'assets/platform.png');
 
 };
