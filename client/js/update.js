3b83d9d6ae83d73cd7b08ac8c2f5b67767772109
knockoutdonkey <chrislockwood92@gmail.com>
2015-08-10 14:27:03 -0700

    created a new jump animation that plays whenever a player jumps

--- client/js/update.js
+++ client/js/update.js
@@ -23,14 +23,10 @@ var update = function(){
 
   if(cursors.left.isDown && player.body.velocity.x > -playerMaxSpeed) {
     player.body.velocity.x -= playerAccleration;
-    
-    player.animations.play('walking');
     player.scale.x = 2;
 
   } else if (cursors.right.isDown && player.body.velocity.x < playerMaxSpeed) {
     player.body.velocity.x += playerAccleration;
-    
-    player.animations.play('walking');
     player.scale.x = -2;
 
   } else {
@@ -41,6 +37,16 @@ var update = function(){
       player.body.velocity.x = Math.max(player.body.velocity.x - playerDecceleration, 0);
 
     } else {
+      if (player.body.touching.down) {
+        player.frame = 0;
+      }
+    }
+  }
+
+  if (player.body.touching.down) {
+    if (player.body.velocity.x !== 0) {
+      player.animations.play('walking');
+    } else {
       player.frame = 0;
     }
   }
@@ -49,12 +55,18 @@ var update = function(){
   // player.animations.currentAnim.speed = Math.abs(player.body.velocity.x / 8) + 5;
 
   // Takes almost no processing, but looks worse
-  player.animations.currentAnim.delay = Math.min(1 / (Math.abs(player.body.velocity.x) * 0.00009), 100);
+
+  if (player.body.touching.down) {
+    player.animations.currentAnim.delay = Math.min(1 / (Math.abs(player.body.velocity.x) * 0.00009), 100);
+  }
 
   // Jump if on ground and move upward until jump runs out or lets go of space
   var jumpSpeed = -850;
   if(jumpButton.isDown && player.body.touching.down) {
     player.body.velocity.y = jumpSpeed;
+    player.animations.stop();
+    player.frame = 24; 
+
   } else if (!jumpButton.isDown && !player.body.touching.down) {
     if (player.body.velocity.y < 0 && player.body.velocity.y > jumpSpeed + 100) {
       player.body.velocity.y = 0;
