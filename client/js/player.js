a155e56018a10102fb4e20749bbb1111bae531f1
knockoutdonkey <chrislockwood92@gmail.com>
2015-08-13 16:20:39 -0700

    Removed a test line

--- client/js/player.js
+++ client/js/player.js
@@ -136,8 +136,6 @@ Player = function(game, x, y, socketId) {
     jumpSpeed = -700 * Math.sqrt(bonus);
   };
 
-  console.log("player made");
-
 };
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
