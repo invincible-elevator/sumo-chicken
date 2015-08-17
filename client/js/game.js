30552c02fa6f7ab264b7df6d7c433bf76aa75782
Shelley Wang <shelleyw13@gmail.com>
2015-08-17 11:11:58 -0700

    Add login page before game loads

--- client/js/game.js
+++ client/js/game.js
@@ -1,9 +1,17 @@
-var game = new Phaser.Game(window.innerWidth, 
-                           window.innerHeight, 
-                           Phaser.AUTO, 
-                           '', 
-                           { preload: preload, 
-                             create: create, 
-                             update: update}
-                          );
+var startGame = function() {
+  console.log(document.getElementById('username').value);
 
+  var main = document.getElementById('main');
+  main.parentNode.removeChild(main);
+
+  game = new Phaser.Game(window.innerWidth, 
+                             window.innerHeight, 
+                             Phaser.AUTO, 
+                             '', 
+                             { preload: preload, 
+                               create: create, 
+                               update: update}
+                            );
+
+  return false;
+};
